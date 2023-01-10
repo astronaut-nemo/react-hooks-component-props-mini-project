381838cdb3777300656d70959fd2d4200d82fd82
Joanna <astronautnemo@gmail.com>
2023-01-10 18:20:42 +0300

    completed bonus deliverable

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
-function Article({title, date="January 1, 1970", preview}){
+// Component Function
+function Article({title, date="January 1, 1970", preview, minutes}){
+    console.log(minutes)
+
+    // Helper function
+    function EmojiMinutes(){
+        if(minutes<30){
+            // const numberOfCoffees = minutes/5;
+            const emojiArray = [];
+            for(let i = 0; i < minutes/5; i++){
+                emojiArray.push("☕")
+            }
+            return (`${emojiArray.join('')} | ${minutes} min read`)
+        }
+        else if(minutes>=30){
+            const emojiArray = [];
+            for(let i = 0; i < minutes/10; i++){
+                emojiArray.push("🍱")
+            }
+            return (`${emojiArray.join('')} | ${minutes} min read`)
+        }
+    }
+
     return(
         <article>
             <h3>{title}</h3>
             <small>{date}</small>
             <p>{preview}</p>
+            {EmojiMinutes()}
         </article>
     )
 };
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,11 +3,11 @@ import Article from "./Article";
 
 
 function ArticleList({posts}){
-    console.log(posts)
+    // console.log(posts)
     
     // Mapping out each post in the list
     const postsList = posts.map((post) =>{
-            return <Article key={post.id} title={post.title} date={post.date} preview={post.preview}/>
+            return <Article key={post.id} title={post.title} date={post.date} preview={post.preview} minutes={post.minutes}/>
     })
 
     return(
