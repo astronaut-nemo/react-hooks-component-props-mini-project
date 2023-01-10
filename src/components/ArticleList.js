b00f6d316f682ae19792fd03b40f54f6b959fb8a
Joanna <astronautnemo@gmail.com>
2023-01-10 17:46:13 +0300

    completed deliverables

---
+++ src/components/ArticleList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Article from "./Article";
+
+
+function ArticleList({posts}){
+    console.log(posts)
+    
+    // Mapping out each post in the list
+    const postsList = posts.map((post) =>{
+            return <Article key={post.id} title={post.title} date={post.date} preview={post.preview}/>
+    })
+
+    return(
+        <main>
+            {postsList}
+        </main>
+    )
+};
+
+export default ArticleList
